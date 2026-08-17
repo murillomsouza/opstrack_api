c0a583d1209b9c7426577321716b2b07bc61815b
murillomsouza <murillosouza997@gmail.com>
2026-08-17 08:53:46 -0300

    feat: implements new endpoints: '/sobre', 'tickets', '/status'

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
+@app.route('/status')
 def status():
     return{'servico': 'OpsTrackAPI', 'status': 'online'}
 
+@app.route('/sobre')
+def sobre():
+    return{'OpsTrack API - Version: 1.0.0'}
+
+@app.route('/tickets')
+def tickets():
+    return{'Ana: 10:07:45 - Mouse sem funcionar\n'
+           'Cassio: 11:37:86 - Impressora travada\n'
+           'Roberto: 12:26:32 - Sem sinal de internet'}
 if __name__ == '__main__':
     app.run(debug=True)
