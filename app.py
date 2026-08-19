8929fac8717e1b99603fca4f11b577f85b01e9d5
murillomsouza <murillosouza997@gmail.com>
2026-08-19 09:19:49 -0300

    fix: corrige erro de json das rotas '/sobre' e '/tickets'

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def status():
 
 @app.route('/sobre')
 def sobre():
-    return{'OpsTrack API - Version: 1.0.0'}
+    return{'version':'OpsTrack API - Version: 1.0.0'}
 
 @app.route('/tickets')
 def tickets():
-    return{'Ana: 10:07:45 - Mouse sem funcionar\n'
-           'Cassio: 11:37:86 - Impressora travada\n'
-           'Roberto: 12:26:32 - Sem sinal de internet'}
+    return{'chamado':'Ana: 10:07:45 - Mouse sem funcionar',
+           'chamado2':'Cassio: 11:37:86 - Impressora travada',
+           'chamado3':'Roberto: 12:26:32 - Sem sinal de internet'}
 if __name__ == '__main__':
     app.run(debug=True)
